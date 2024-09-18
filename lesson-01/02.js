e892e17ce3de27ff8c5b126eb567d5fcda05764e
Sergey <97605282+Sergey-Egorov-web@users.noreply.github.com>
2024-09-18 21:53:53 +0300

    Task 3

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -5,3 +5,13 @@ const result =
   number1 % 2 == 0
     ? console.log(`${number1} :это четное число`)
     : console.log(`${number1} :это нечетное число`);
+
+let number = 1;
+console.log(number);
+
+number++;
+console.log(++number);
+number++;
+console.log(--number);
+number--;
+console.log(--number);

d7e0147ffedb9a0fae0dc77dd22da713199b1bb8
Sergey <97605282+Sergey-Egorov-web@users.noreply.github.com>
2024-09-18 06:51:44 +0300

    2 task

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,8 @@
 // code
+let number1 = Math.floor(Math.random() * 100);
+console.log(number1);
+if (number1 % 2 == 0) {
+  console.log(`${number1} это чётное число`);
+} else {
+  console.log(`${number1} это нечётное число`);
+}

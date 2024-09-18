0310531cab2353eaa5f66912e58504e53e6f1721
Sergey <97605282+Sergey-Egorov-web@users.noreply.github.com>
2024-09-18 20:57:15 +0300

    Add files via upload

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,8 +1,7 @@
 // code
 let number1 = Math.floor(Math.random() * 100);
 
-if (number1 % 2 == 0) {
-  console.log(`${number1} :это чётное число`);
-} else {
-  console.log(`${number1} :это нечётное число`);
-}
+const result =
+  number1 % 2 == 0
+    ? console.log(`${number1} :это чётное число`)
+    : console.log(`${number1} :это нечётное число`);

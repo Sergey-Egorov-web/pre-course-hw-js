3370b344e0a352c4aff107e9ea7c3f1c3716deb4
Sergey <97605282+Sergey-Egorov-web@users.noreply.github.com>
2024-09-25 16:32:48 +0300

    Add files via upload

---
+++ lesson-02/05.js
@@ -0,0 +1,12 @@
+let passport = {
+  name: "Petr",
+  surname: "Petrov",
+};
+
+let passportCopy = {
+  name: "Ivan",
+  surname: "Petrov",
+};
+
+console.log(passport.name);
+console.log(passportCopy.name);
